36072419a945a200251c4721fcde716cb5a9a698
Crafterboy27-school <95499245+Crafterboy27-school@users.noreply.github.com>
2022-08-30 08:21:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var saveText = 0;
 var icecream = 0;
 var chocalateIcecream = 0;
 var icecreamEarnedPerSecond = 0;
+//
+if (document.cookies != null){
+  loadSave(document.cookies);
+}
 
 //objects/lists
 const saveData = {
@@ -17,6 +21,7 @@ var yourBuildings =
   "grandpas": 1,
   "icecream machine": 1,
   "milk farm and proccesing room": 1,
+  "icecream factory": 1,
 }
 
 const buildingStartCost =
@@ -24,6 +29,7 @@ const buildingStartCost =
   "grandpas": 5,
   "icecream machine": 50,
   "milk farm and proccesing room": 300,
+  "icecream factory": 1500,
 }
 
 const buildingIcecreamPerSecond =
@@ -31,11 +37,12 @@ const buildingIcecreamPerSecond =
   "grandpas": 0.3,
   "icecream machine": 3,
   "milk farm and proccesing room": 6,
+  "icecream factory": 48,
 }
 
 
-//functions
-function buyNewBuilding(buildingName) {
+//function
+function buyNewBuilding(buildingName) {  //spaghett code I forgot how it works
   console.log("buying...")
   if (icecream >= buildingStartCost[buildingName] * yourBuildings[buildingName]) {
     console.log("successfuly bought: " + buildingName);
@@ -52,7 +59,7 @@ function renderIcecreamAmount() {
   document.getElementById("icecreamAmount").innerHTML = "Icecream: ".concat(Math.round(icecream));
 }
 
-function renderBuilding(buildingName) {
+function renderBuilding(buildingName) { //spaghett code I forgot how it works
   let yourBuildingsLocalDisplay = yourBuildings[buildingName] - 1;
   document.getElementById(buildingName).innerHTML = buildingName + "--- cost in icecream: " + buildingStartCost[buildingName] * yourBuildings[buildingName] + " how many you own: " + yourBuildingsLocalDisplay;
 }
@@ -71,11 +78,13 @@ function clicktest() {
 
 
 function save(showOutput){
+  
 
   saveData.icecream = icecream;
   saveData.buildings = yourBuildings;
   saveData.icecreamPerSecond = icecreamEarnedPerSecond;
   saveText = btoa( JSON.stringify( saveData ) );
+
 }
   
   
@@ -100,6 +109,7 @@ function save(showOutput){
   function showSaveInSaveBox() {
    save()
    document.getElementById("saveCode").value = saveText;
+   document.cookies = saveText
   }
 
 
@@ -108,17 +118,25 @@ function save(showOutput){
     renderIcecreamAmount()
   }
 
+
+
+
+
 //loops
 function loopHandler(){
 
   setInterval(function() {
     autoClick()
     showSaveInSaveBox()
+    save()
+    document.cookies = saveText
+
   }, 1000);
 
 }
 
 //game startup stuff
+
 
 //render
 function startRender(){
@@ -126,9 +144,18 @@ renderIcecreamAmount()
 renderBuilding("grandpas")
 renderBuilding("icecream machine")
 renderBuilding("milk farm and proccesing room")
+renderBuilding("icecream factory")
 
+}
+
+if (document.cookies == null){
+  print("cookies not detected")
+}else{
+  print("cookies detected")
+  loadSave(document.cookies)
 }
 startRender()
 //start loops
 loopHandler()
+
 
